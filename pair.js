2c96e80dfb449b6c99d6dedab7c52ea7d066e08c
sungdev-cpu <vertexxcode@gmail.com>
2025-08-16 19:05:09 +0200

    Update pair.js

--- pair.js
+++ pair.js
@@ -52,14 +52,14 @@ router.get('/', async (req, res) => {
                     let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                     await delay(800);
                     let b64data = Buffer.from(data).toString('base64');
-                    let session = await Pair_Code_By_Malvin_Tech.sendMessage(Pair_Code_By_Malvin_Tech.user.id, { text: 'starcore~' + b64data });
+                    let session = await Pair_Code_By_Malvin_Tech.sendMessage(Pair_Code_By_Malvin_Tech.user.id, { text: 'cyberia~' + b64data });
 
                     let Star_MD_TEXT = `
 
 ╭─═━⌬━═─⊹⊱✦⊰⊹─═━⌬━═─ 
 ╎   『 𝐒𝐄𝐒𝐒𝐈𝐎𝐍 𝐂𝐎𝐍𝐍𝐄𝐂𝐓𝐄𝐃 』   
 ╎  ✦ sᴛᴀʀᴄᴏʀᴇ sᴇssɪᴏɴ
-╎  ✦  ʙʏ ᴅᴇᴠ ᴍᴀʟᴠɪɴ
+╎  ✦  ʙʏ ᴅᴇᴠ sung
 ╰╴╴╴╴
 
 ▌   『 🔐 𝐒𝐄𝐋𝐄𝐂𝐓𝐄𝐃 𝐒𝐄𝐒𝐒𝐈𝐎𝐍 』   
@@ -69,15 +69,15 @@ router.get('/', async (req, res) => {
 ╔═
 ╟   『 𝐂𝐎𝐍𝐓𝐀𝐂𝐓 & 𝐒𝐔𝐏𝐏𝐎𝐑𝐓 』  
 ╟  🎥 𝐘𝐨𝐮𝐓𝐮𝐛𝐞: youtube.com/@malvintech2  
-╟  👑 𝐎𝐰𝐧𝐞𝐫: 263714757857  
-╟  💻 𝐑𝐞𝐩𝐨: github.com/XdKing2/Star-xd
-╟  💻 𝐑𝐞𝐩𝐨: github.com/XdKing2/MALVIN-XD  
+╟  👑 𝐎𝐰𝐧𝐞𝐫: (num unavailable due to spam)  
+╟  💻 𝐑𝐞𝐩𝐨: github.com/NaCkS-ai2/Cyberia-MD
+╟       
 ╟  👥 𝐖𝐚𝐆𝐫𝐨𝐮𝐩: https://chat.whatsapp.com/Dx7HbtW7Cf12iCVjJBpD0x?mode=ac_t 
 ╟  📢 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: https://whatsapp.com/channel/0029VbB3YxTDJ6H15SKoBv3S 
 ╟  📸 𝐈𝐧𝐬𝐭𝐚: instagram.com/techlord01  
 ╰  
 ✦⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅✦  
-   𝐄𝐍𝐉𝐎𝐘 𝐒𝐓𝐀𝐑-𝐗𝐃!  
+   𝐄𝐍𝐉𝐎𝐘 Cyberia-MD!  
 ✦⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅✦  
 ______________________________
 ★彡[ᴅᴏɴ'ᴛ ғᴏʀɢᴇᴛ ᴛᴏ sᴛᴀʀ ᴛʜᴇ ʀᴇᴘᴏ!]彡★
